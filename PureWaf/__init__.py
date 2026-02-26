9019f583419ad46b9315063adc977007d8c21725
1262364207@qq.com <PureStream108@users.noreply.github.com>
2026-02-26 19:01:09 +0800

    更新了版本

--- PureWaf/__init__.py
+++ PureWaf/__init__.py
@@ -1,3 +1,5 @@
-from .PureWaf import banner, purewaf, version
+from .PureWaf import banner, purewaf, register_update_notice_at_exit, version
+
+register_update_notice_at_exit()
 
 __all__ = ["purewaf", "version", "banner"]
