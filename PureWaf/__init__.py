554dbe5174d37b8777e48a83736d84a6cdaf21ca
PureStream108 <PureStream108@users.noreply.github.com>
2026-04-08 12:26:01 +0800

    删除了版本更新相关

--- PureWaf/__init__.py
+++ PureWaf/__init__.py
@@ -1,5 +1,3 @@
-from .PureWaf import banner, purewaf, register_update_notice_at_exit, version
-
-register_update_notice_at_exit()
+from .PureWaf import banner, purewaf, version
 
 __all__ = ["purewaf", "version", "banner"]
